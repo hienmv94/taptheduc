8f8fa54f5691dff4ff530c7caae9d778c352c6b0
dongpp57 <dongpp57@gmail.com>
2015-12-12 23:09:28 +0700

    Search done

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'demo'),
+            'database'  => env('DB_DATABASE', 'db_diadiem'),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
